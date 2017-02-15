f2b454af7e54ef8104dcad4ca32d33551d1cf476
John Byrne <usscork@gmail.com>
2017-02-15 19:26:03 +0000

    RC, no comments

--- main.c
+++ main.c
@@ -7,11 +7,8 @@
 #define INPUT_DATA_FILE "../sortedData.dat"
 
 void loadFileData(float arrayToStoreData[], size_t arraySize);
-
-
 void categorizeData(float sourceDataArray[], int sourceArraySize, int frequencyDistributionArray[5]);
-
-void displayDataToUser(int frequencyDistributionDataArray[5]);
+void displayDataToUser(int frequencyDistributionDataArray[5], float sourceDataArray[], size_t sourceArraySize);
 
 int main() {
 
@@ -23,27 +20,41 @@ int main() {
 
     categorizeData(windSpeedData, NUMBER_OF_DATAPOINTS, frequencyDistribution); //Categorise my data into 5 equal buckets.
 
-    displayDataToUser(frequencyDistribution);
-
-
+    displayDataToUser(frequencyDistribution, windSpeedData, NUMBER_OF_DATAPOINTS);
 
     return 0;
 }
 
-void displayDataToUser(int frequencyDistributionDataArray[5]) {
+void displayDataToUser(int frequencyDistributionDataArray[5], float sourceDataArray[], size_t arrayDataSize) {
 
-    puts("\n");
-    puts("\n Histogram showing Distribution of windspeeds on 22nd September 2013");
-    puts("           each '*' represents up to 10 measurements\n");
+    //If my data was not already sorted by the previous exercise, then I'd have to iterate through
+    //the list to identify the min and max values. But since it is already sorted, then...
+    float min = sourceDataArray[0];   //The min value is the first item in my sorted list.
+    float max = sourceDataArray[arrayDataSize - 1]; //The max value is the last value. 1440 data items, numbered 0 -> 1439.
+
+    int sampleCount = 0;    //Should be 1440 for this exercise, But could be different if this function was processing other arrays.
+
+    ////TESTING
+    //printf("The minimum wind speed was %.3f m/s\n The maximum MAX data was %f.\n", min, max);
+
+
+    puts("\nHistogram showing Distribution of wind speeds on 22nd September 2013");
+    puts("           each '*' represents up to 10 measurements\n\n");
+    printf("Minimum recorded wind speed was: %6.3f m/s.\n", min);
+    printf("Maximum recorded wind speed was: %6.3f m/s.\n\n", max);
     printf("%9s%12s\n", "% of MAX", "Frequency");
     for (int x = 0; x < 5; x++){
+        sampleCount+=frequencyDistributionDataArray[x];
         printf("%2d  -> %3d%8d   |", 100 - (20 * (x + 1)), 100 - (20 * x), frequencyDistributionDataArray[x]);		//Bar label. Contains the number being represented.
         for (int count = 1; count < frequencyDistributionDataArray[x]; count+=10) {	//Repeat num(x) times...
             printf("%s", "*");							//Print an '*'
         }//End of for
         puts("");
     }
-    printf("%22s%5d%5d%5d%5d%5d%5d%5d%5d%5d%5d\n", "", 5, 1, 1, 2, 2, 3, 3, 4, 4, 5);
+
+
+    printf("%11s%7d", "Total: ", sampleCount);
+    printf("%4s%5d%5d%5d%5d%5d%5d%5d%5d%5d%5d\n",  "", 5, 1, 1, 2, 2, 3, 3, 4, 4, 5);
     printf("%22s%5d%5d%5d%5d%5d%5d%5d%5d%5d%5d\n", "", 0, 0, 5, 0, 5, 0, 5, 0, 5, 0);
     printf("%27s%5d%5d%5d%5d%5d%5d%5d%5d%5d\n",    "",    0, 0, 0, 0, 0, 0, 0, 0, 0);
 
@@ -56,15 +67,17 @@ void categorizeData(float sourceDataArray[],
 
     //If my data was not already sorted by the previous exercise, then I'd have to iterate through
     //the list to identify the min and max values. But since it is already sorted, then...
-    float min = sourceDataArray[0];   //The min value is the first item in my sorted list.
+    //float min = sourceDataArray[0];   //The min value is the first item in my sorted list.
     float max = sourceDataArray[sourceArraySize - 1]; //The max value is the last value. 1440 data items, numbered 0 -> 1439.
+    //float max = 0.0;
 
     ////TESTING
-    printf("The min data was %f and the MAX data was %f.\n\n\n", min, max);
+//    printf("The minimum wind speed was %.3f m/s\n The maximum MAX data was %f.\n", min, max);
 
 
 
     for(size_t index = 0; index < sourceArraySize; index++){
+        //if(sourceDataArray[index] > max) max = sourceDataArray[index];
         if (sourceDataArray[index] > max * 0.8){
             frequencyDistributionArray[4]++;
         }else if(sourceDataArray[index] > max * 0.6){
